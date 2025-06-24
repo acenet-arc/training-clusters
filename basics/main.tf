d421476c79d895ef1e3f75f5f6d0bcf3eb2e2b19
Serguiei Vassiliev <svassiliev1@gmail.com>
2025-06-24 08:51:16 -0300

    MC for UNB SWC

--- basics/main.tf
+++ basics/main.tf
@@ -19,7 +19,7 @@ module "openstack" {
   instances = {
     mgmt   = { type = "p8-15gb", tags = ["puppet", "mgmt", "nfs"], count = 1}
     login  = { type = "p8-15gb", tags = ["login", "public", "proxy"], count = 1}
-    node2c-   = { type = "c2-7.5gb", tags = ["node"], count = 2 }
+    node2c-   = { type = "c2-7.5gb", tags = ["node"], count = 1 }
   }
 
   # var.pool is managed by Slurm through Terraform REST API.
