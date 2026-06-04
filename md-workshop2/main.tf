7be514d7ba791c310ed0f4402e838ea96cb64565
Chris Geroux <chris.m.geroux@gmail.com>
2026-06-04 09:23:47 -0300

    Added new md-workshop2 config for new arbutus

---
+++ md-workshop2/main.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_version = ">= 1.5.7"
+}
+
+variable "pool" {
+  description = "Slurm pool of compute nodes"
+  default = []
+}
+
+module "openstack" {
+  source         = "git::https://github.com/ComputeCanada/magic_castle.git//openstack?ref=15.4.1"
+  config_git_url = "https://github.com/ComputeCanada/puppet-magic_castle.git"
+  config_version = "15.4.1"
+
+  cluster_name = "md-workshop"
+  domain       = "ace-net.training"
+  image        = "Rocky-9-x64-2025-08"
+
+  instances = {
+    mgmt   = { type = "p8-12gb", tags = ["puppet", "mgmt", "nfs"], count = 1}
+    login  = { type = "p8-12gb", tags = ["login", "public", "proxy"], count = 1}
+    node8c-   = { type = "cb8-30gb-280", tags = ["node"], count = 1 }
+    node3c1g-   = { type = "g1-12gb-c3-35gb-125", tags = ["node"], count = 1 }
+  }
+
+  # var.pool is managed by Slurm through Terraform REST API.
+  # To let Slurm manage a type of nodes, add "pool" to its tag list.
+  # When using Terraform CLI, this parameter is ignored.
+  # Refer to Magic Castle Documentation - Enable Magic Castle Autoscaling
+  pool = var.pool
+
+  volumes = {
+    nfs = {
+      home     = { size = 100 }
+      project  = { size = 50 }
+      scratch  = { size = 50 }
+    }
+  }
+
+  public_keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWHSMDMhlXIy+C7/Dw4b7dUgfZkE3AXnG8PDDkyY9Qm cgeroux@lunar","ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKNJSw9+yFmtwRpJzoagrQEzobMkMEQxGLqs7edjNJZS 2021-07-28 ostueker@ACE-Helium","ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOCI9Gh8K6m1V+qpSoyp9tBmMCvtk0uY4rUD+6GaWgAX rdickson@BRTJTQ2","ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBjz+roGXPS1iY+/dPbTAsQcGSYjv1XPvtJP/CoN3V38 svassili@Sergueis-Air", "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBblyJ+6JynjS7kxzawodNvRrOTGVGj7266zcFJuq01N 1password_ed25519"]
+
+  nb_users = 100
+  # Shared password, randomly chosen if blank
+  guest_passwd = ""
+  
+  hieradata = file("./config.yml")
+}
+
+output "accounts" {
+  value = module.openstack.accounts
+}
+
+output "public_ip" {
+  value = module.openstack.public_ip
+}
+
+## Uncomment to register your domain name with CloudFlare
+module "dns" {
+   source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/cloudflare"
+   name             = module.openstack.cluster_name
+   domain           = module.openstack.domain
+   public_instances = module.openstack.public_instances
+}
+
+## Uncomment to register your domain name with Google Cloud
+# module "dns" {
+#   source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/gcloud"
+#   project          = "your-project-id"
+#   zone_name        = "you-zone-name"
+#   name             = module.openstack.cluster_name
+#   domain           = module.openstack.domain
+#   public_instances = module.openstack.public_instances
+# }
+
+output "hostnames" {
+  value = module.dns.hostnames
+}
